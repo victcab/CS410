3faed0d8ca83b8366f12d17f5eede513c82f6299
Alex <alex_llamas@icloud.com>
2019-05-21 12:47:58 -0700

    pillow implementation

--- main.py
+++ main.py
@@ -1,1 +1,21 @@
-print ("Hello world")
+from PIL import Image
+
+
+def main():
+    try:
+        print("trying")
+        # Relative Path
+        img = Image.open("testing.jpg")
+
+        # Angle given
+        img = img.rotate(180)
+
+        # Saved in the same relative location
+        img.save("rotated_picture.jpg")
+    except IOError:
+        print("ERROR")
+        pass
+
+
+if __name__ == "__main__":
+    main()
