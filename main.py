d15dd87dfd9932764114b656f25d9a2ba40d4df2
Vic <cabunoc@pdx.edu>
2019-05-25 12:52:37 -0700

    Working on detecting color

--- main.py
+++ main.py
@@ -5,53 +5,47 @@ import numpy as np
 
 sourceImg = cv2.imread('testing.jpg')
 height, width, channels = sourceImg.shape;
-cv2.imshow("sourceImage",sourceImg)
 
-blurImg = cv2.blur(sourceImg, (5, 5))
+img = cv2.imread('testing.jpg')
+
+val = int(input("Enter your level of blur:"))
+
+blurImg = cv2.blur(sourceImg, (val, val))
+#gray = cv2.cvtColor(blurImg,cv2.COLOR_BGR2GRAY)
+#red = cv2.cvtColor(blurImg,cv2.COLOR_BGR2RGB)
+#ret, thresh = cv2.threshold(gray,0,255,cv2.THRESH_BINARY_INV+cv2.THRESH_OTSU)
+
+####################################################
+hsv = cv2.cvtColor(blurImg, cv2.COLOR_BGR2HSV)
+mask = cv2.inRange(hsv, (36, 25, 25), (70, 255,255))
+imask = mask>0
+green = np.zeros_like(blurImg, np.uint8)
+green[imask] = blurImg[imask]
+cv2.imwrite("green.png", green)
+####################################################
+mask = cv2.inRange(hsv, (120, 25, 25), (270, 255, 255))
+imask = mask>0
+pink = np.zeros_like(blurImg, np.uint8)
+pink[imask] = blurImg[imask]
+cv2.imwrite("pink.png", pink)
+####################################################
+mask = cv2.inRange(hsv, (10, 25, 25), (30, 255, 255))
+imask = mask>0
+brown = np.zeros_like(blurImg, np.uint8)
+brown[imask] = blurImg[imask]
+cv2.imwrite("brown.png", brown)
+#####################################################
+
+cv2.imshow('og', img)
 cv2.imshow('blurred image', blurImg)
+cv2.imshow('green', green)
+cv2.imshow('pink', pink)
+cv2.imshow('brown', brown)
 
 blankCanvas = np.zeros((height,width,3), np.uint8)
-blankCanvas[:] = (245, 245, 220)
-cv2.imshow("blankCanvas",blankCanvas)
+
+#blankCanvas[:] = (245, 245, 220)
+#cv2.imshow("blankCanvas",blankCanvas)
 
 cv2.waitKey(0)
 cv2.destroyAllWindows()
-
-
-
-# # from PIL import Image
-# # from PIL import ImageFilter
-# cv2.waitKey()
-# cv2.destroyAllWindows()
-
-
-# def main():
-#     # try:
-#         # print("trying")
-#         # # Relative Path
-#         # img = Image.open("testing2.jpg")
-#         #
-#         #
-#         # constantImage = Image.new('RGB', (500, 500), "black")
-#         #
-#         # blurred_image = img.filter(ImageFilter.GaussianBlur(radius=100))
-#         # # pixels = img.load()  # create the pixel map
-#         # #
-#         # # for i in range(img.size[0]):  # for every col:
-#         # #     for j in range(img.size[1]):  # For every row
-#         # #         pixels[i, j] = (255, 255, 255)  # set the colour accordingly
-#         #         #print(pixels[i,j])
-#         #         #print("hi")
-#         #
-#         # img.show()
-#         # constantImage.show()
-#         # blurred_image.show()
-#
-#
-#     # except IOError:
-#     #     print("ERROR")
-#     #     pass
-#
-# #
-# # if __name__ == "__main__":
-# #     main()
