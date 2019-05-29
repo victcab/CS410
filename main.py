9930ebc49a383c2d7144658d1c5e73a262b564c1
Vic <cabunoc@pdx.edu>
2019-05-29 08:56:24 -0700

    Testing HSV

--- main.py
+++ main.py
@@ -2,50 +2,36 @@
 import cv2
 import numpy as np
 
-def CreateColorRange(lowerlimit, upperlimit, color):
-    mask = cv2.inRange(blurImg, (lowerlimit, 0, 0), (upperlimit, 204, 204))
+def CreateColorRange(hue, sat, val):
+    mask = cv2.inRange(blurImg, (hue-10, sat-10, val-10), (hue+10, sat+10, val+10))
     imask = mask>0
     color = np.zeros_like(blurImg, np.uint8)
     color[imask] = blurImg[imask]
     return color
 
 img = cv2.imread('testing.jpg')
-height, width, channels = img.shape;
+height, width, channels = img.shape
 val = int(input("Enter your level of blur:"))
 blurImg = cv2.blur(img, (val, val))
-#hsv = cv2.cvtColor(blurImg, cv2.COLOR_BGR2HSV)
-
-#gray = cv2.cvtColor(blurImg,cv2.COLOR_BGR2GRAY)
-#red = cv2.cvtColor(blurImg,cv2.COLOR_BGR2RGB)
-#ret, thresh = cv2.threshold(gray,0,255,cv2.THRESH_BINARY_INV+cv2.THRESH_OTSU)
+hsv = cv2.cvtColor(blurImg, cv2.COLOR_BGR2HSV)
 
 #######################################5#############
-#red = CreateColorRange(102, 255, 'red')
+green = CreateColorRange(60, 100, 50)
+#orange = CreateColorRange(
+#yellow = CreateColorRange(
+#green = CreateColorRange(
+#lightblue = CreateColorRange(
+#darkblue = CreateColorRange(
+#purple = CreateColorRange(
 
-mask = cv2.inRange(blurImg, (0, 255, 0), (250, 255, 250))
-imask = mask>0
-red = np.zeros_like(blurImg, np.uint8)
-red[imask] = blurImg[imask]
+#blankCanvas = np.zeros((height,width,3), np.uint8)
 
-#orange = CreateColorRange(10, 20, 'orange')
-#yellow = CreateColorRange(20, 30, 'yellow')
-#green = CreateColorRange(30, 70, 'green')
-#lightblue = CreateColorRange(70, 95, 'lightblue')
-#darkblue = CreateColorRange(95, 135, 'darkblue')
-#purple = CreateColorRange(135, 175, 'purple')
-
-#mask = cv2.inRange(blurImg, (0, 0, 255), (255, 0, 255))
-#imask = mask>0
-#white = np.zeros_like(blurImg, np.uint8)
-#white[imask] = blurImg[imask]
-
-blankCanvas = np.zeros((height,width,3), np.uint8)
 #cv2.imshow('og', img)
 cv2.imshow('blurred image', blurImg)
-cv2.imshow('red', red)
+#cv2.imshow('red', red)
 #cv2.imshow('orange', orange)
 #cv2.imshow('yellow', yellow)
-#cv2.imshow('green', green)
+cv2.imshow('green', green)
 #cv2.imshow('lightblue', lightblue)
 #cv2.imshow('darkblue', darkblue)
 #cv2.imshow('purple', purple)
