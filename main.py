5ca3e915f2db1c0c3c9238bfc011b27a7bb1f916
Vic <cabunoc@pdx.edu>
2019-05-29 15:53:51 -0700

    Able to detect colors but it sucks

--- main.py
+++ main.py
@@ -2,8 +2,8 @@
 import cv2
 import numpy as np
 
-def CreateColorRange(hue, sat, val):
-    mask = cv2.inRange(blurImg, (hue-10, sat-10, val-10), (hue+10, sat+10, val+10))
+def CreateColorRange(hlower, slower, vlower, hupper, supper, vupper):
+    mask = cv2.inRange(hsv, (hlower, slower, vlower), (hupper, supper, vupper))
     imask = mask>0
     color = np.zeros_like(blurImg, np.uint8)
     color[imask] = blurImg[imask]
@@ -16,27 +16,54 @@ blurImg = cv2.blur(img, (val, val))
 hsv = cv2.cvtColor(blurImg, cv2.COLOR_BGR2HSV)
 
 #######################################5#############
-green = CreateColorRange(60, 100, 50)
-#orange = CreateColorRange(
-#yellow = CreateColorRange(
-#green = CreateColorRange(
-#lightblue = CreateColorRange(
-#darkblue = CreateColorRange(
-#purple = CreateColorRange(
+black = CreateColorRange(0,0,0,0,0,0)
+white = CreateColorRange(0,0,127.5,0,0,255)
+gray = CreateColorRange(0,0,0,0,0,127.5)
+
+orange1 = CreateColorRange(10, 75, 127.5, 22, 255, 255)
+orange2 = CreateColorRange(10, 75, 0, 22, 255, 127.5)
+
+yellow1 = CreateColorRange(22, 75, 127.5, 32, 255, 255)
+yellow2 = CreateColorRange(22, 75, 0, 32, 255, 127.5)
+
+lightgreen = CreateColorRange(40, 75, 127.5, 72, 255, 255)
+darkgreen = CreateColorRange(40, 75, 0, 72, 255, 127.5)
+
+lightcyan = CreateColorRange(82, 75, 127.5, 100, 255, 255)
+darkcyan = CreateColorRange(82, 75, 0, 100, 255, 127.5)
+
+lightblue = CreateColorRange(115, 75, 127.5, 125, 255, 255)
+darkblue = CreateColorRange(115, 75, 0, 125, 255, 127.5)
+
+lightred = CreateColorRange(161, 75, 127.5, 180, 255, 255)
+darkred = CreateColorRange(161, 75, 0, 180, 255, 127.5)
+
 
 #blankCanvas = np.zeros((height,width,3), np.uint8)
 
 #cv2.imshow('og', img)
 cv2.imshow('blurred image', blurImg)
-#cv2.imshow('red', red)
-#cv2.imshow('orange', orange)
-#cv2.imshow('yellow', yellow)
-cv2.imshow('green', green)
-#cv2.imshow('lightblue', lightblue)
-#cv2.imshow('darkblue', darkblue)
-#cv2.imshow('purple', purple)
-#cv2.imshow('white', white)
+#############################################################################
+cv2.imshow('black', black)
+cv2.imshow('white', white)
+cv2.imshow('gray', gray)
 
+cv2.imshow('orange1', orange1)
+cv2.imshow('orange2', orange2)
+
+cv2.imshow('yellow1', yellow1)
+cv2.imshow('yellow2', yellow2)
+
+cv2.imshow('lightgreen', lightgreen)
+cv2.imshow('darkgreen', darkgreen)
+cv2.imshow('lightred', lightred)
+cv2.imshow('darkred', darkred)
+
+cv2.imshow('lightblue', lightblue)
+cv2.imshow('darkblue', darkblue)
+
+cv2.imshow('lightcyan', lightcyan)
+cv2.imshow('darkcyan', darkcyan)
 #blankCanvas[:] = (245, 245, 220)
 #cv2.imshow("blankCanvas",blankCanvas)
 
