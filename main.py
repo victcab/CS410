6dd95c0343300ed46051f1c0885727ecd62042c0
Alex <alex_llamas@icloud.com>
2019-05-29 09:16:23 -0700

    circle

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
-# importing opencv CV2 module
 import cv2
 import numpy as np
+
+
 
 def main():
     print("main function")
     sourceImg = cv2.imread('testing.jpg')
-    paint(sourceImg, [28, 20, 12, 4])
+    paint(sourceImg, [30, 12, 8, 4, 2])
 
 
 def paint(sourceImg, radii):
@@ -19,65 +20,69 @@ def paint(sourceImg, radii):
     cv2.waitKey(0)
     cv2.destroyAllWindows()
 
+
     blankCanvas = np.zeros((height,width,3), np.uint8)
-    blankCanvas[:] = (245, 245, 220)
+    blankCanvas[:] = (0, 0, 0)
     cv2.namedWindow("blankCanvas");
     cv2.moveWindow("blankCanvas", 40, 30)  # Move it to (40,30)
+
+    cv2.circle(blankCanvas,(100, 300), 100, (120, 100, 50), -1)
+    # for i in range(width):  # for every col:
+    #     for j in range(height):  # For every row
+    #         blankCanvas[i, j] = (100, 100, 100)  # set the colour accordingly
+
+
     cv2.imshow("blankCanvas",blankCanvas)
     cv2.waitKey(0)
     cv2.destroyAllWindows()
 
 
-    while i < len(radii):
-        print(radii[i])
-        blurImg = cv2.blur(sourceImg, (radii[i], radii[i]))
-        cv2.namedWindow("blurred image");
-        cv2.moveWindow("blurred image", 40, 30)  # Move it to (40,30)
-        cv2.imshow('blurred image', blurImg)
-        cv2.waitKey(0)
-        cv2.destroyAllWindows()
-        i += 1
+    # while i < len(radii):
+    #     print(radii[i])
+    #     blurImg = cv2.blur(sourceImg, (radii[i], radii[i]))
+    #     cv2.namedWindow("blurred image");
+    #     cv2.moveWindow("blurred image", 40, 30)  # Move it to (40,30)
+    #     cv2.imshow('blurred image', blurImg)
+    #     cv2.waitKey(0)
+    #     cv2.destroyAllWindows()
+    #     i += 1
 
 
 
- #   cv2.destroyAllWindows()
 
-
-
-# # from PIL import Image
-# # from PIL import ImageFilter
-# cv2.waitKey()
-# cv2.destroyAllWindows()
-
-
-# def main():
-#     # try:
-#         # print("trying")
-#         # # Relative Path
-#         # img = Image.open("testing2.jpg")
-#         #
-#         #
-#         # constantImage = Image.new('RGB', (500, 500), "black")
-#         #
-#         # blurred_image = img.filter(ImageFilter.GaussianBlur(radius=100))
-#         # # pixels = img.load()  # create the pixel map
-#         # #
-#         # # for i in range(img.size[0]):  # for every col:
-#         # #     for j in range(img.size[1]):  # For every row
-#         # #         pixels[i, j] = (255, 255, 255)  # set the colour accordingly
-#         #         #print(pixels[i,j])
-#         #         #print("hi")
-#         #
-#         # img.show()
-#         # constantImage.show()
-#         # blurred_image.show()
+# def paint(sourceImg, radii):
+#
+#     i = 0
+#
+#     height, width, channels = sourceImg.shape;
+#     cv2.namedWindow("sourceImage");
+#     cv2.moveWindow("sourceImage", 40, 30)  # Move it to (40,30)
+#     cv2.imshow("sourceImage",sourceImg)
+#     cv2.waitKey(0)
+#     cv2.destroyAllWindows()
 #
 #
-#     # except IOError:
-#     #     print("ERROR")
-#     #     pass
+#     blankCanvas = np.zeros((height,width,3), np.uint8)
+#     blankCanvas[:] = (255, 255, 255)
+#     cv2.namedWindow("blankCanvas");
+#     cv2.moveWindow("blankCanvas", 40, 30)  # Move it to (40,30)
 #
-# #
+#     cv2.circle(blankCanvas,(100, 300), 100, (120, 100, 50), -1)
+#
+#     cv2.imshow("blankCanvas",blankCanvas)
+#     cv2.waitKey(0)
+#     cv2.destroyAllWindows()
+#
+#
+#     # while i < len(radii):
+#     #     print(radii[i])
+#     #     blurImg = cv2.blur(sourceImg, (radii[i], radii[i]))
+#     #     cv2.namedWindow("blurred image");
+#     #     cv2.moveWindow("blurred image", 40, 30)  # Move it to (40,30)
+#     #     cv2.imshow('blurred image', blurImg)
+#     #     cv2.waitKey(0)
+#     #     cv2.destroyAllWindows()
+#     #     i += 1
 
 if __name__ == "__main__":
     main()
