28cf3f28bd7c7d0fcf123f95bdea22911b6e5154
Alex <alex_llamas@icloud.com>
2019-05-30 16:49:09 -0700

    dots

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import numpy as np
 
 def main():
     print("main function")
-    sourceImg = cv2.imread('testing.jpg')
+    sourceImg = cv2.imread('darkgreen.jpg')
     paint(sourceImg, [30, 12, 8, 4, 2])
 
 
@@ -23,33 +23,39 @@ def paint(sourceImg, radii):
 
     blankCanvas = np.zeros((height,width,3), np.uint8)
     blankCanvas[:] = (0, 0, 0)
-    # color = int(blankCanvas[300, 300])
-    # print(color)
     cv2.namedWindow("blankCanvas");
     cv2.moveWindow("blankCanvas", 40, 30)  # Move it to (40,30)
 
 
-    for i in range(0, height, 20):  # for every col:
-        for j in range(0, width, 20):  # For every row
-             #blankCanvas[j, i] = (255, 255, 255)  # set the colour accordingly
-             cv2.circle(sourceImg, (j, i), 5, (i, j, 1), -1)
+    for i in range(0, height, 7):  # for every col:
+        for j in range(0, width, 7):  # For every row
+            #color = tuple(blankCanvas[300, 300])
+            #blankCanvas[j, i] = (255, 255, 255)  # set the colour accordingly
+
+             if np.any(sourceImg[i,j] != [0, 0, 0]):
+                 cv2.circle(sourceImg, (j, i), 3, (0, 0, 255), -1)
+            #     cv2.circle(blankCanvas, (j, i), 10, (0, 0, 255), -1)
 
 
-    cv2.imshow("blankCanvas",blankCanvas)
+    # cv2.imshow("blankCanvas",blankCanvas)
+    # cv2.waitKey(0)
+    # cv2.destroyAllWindows()
+
+    cv2.imshow("dots",sourceImg)
     cv2.waitKey(0)
     cv2.destroyAllWindows()
 
     i = 0
 
-    while i < len(radii):
-        print(radii[i])
-        blurImg = cv2.blur(sourceImg, (radii[i], radii[i]))
-        cv2.namedWindow("blurred image");
-        cv2.moveWindow("blurred image", 40, 30)  # Move it to (40,30)
-        cv2.imshow('blurred image', blurImg)
-        cv2.waitKey(0)
-        cv2.destroyAllWindows()
-        i += 1
+    # while i < len(radii):
+    #     print(radii[i])
+    #     blurImg = cv2.blur(sourceImg, (radii[i], radii[i]))
+    #     cv2.namedWindow("blurred image");
+    #     cv2.moveWindow("blurred image", 40, 30)  # Move it to (40,30)
+    #     cv2.imshow('blurred image', blurImg)
+    #     cv2.waitKey(0)
+    #     cv2.destroyAllWindows()
+    #     i += 1
 
 
 
