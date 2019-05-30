9b49f41428ac4837e310f4b3eda82ec366826d9f
Alex <alex_llamas@icloud.com>
2019-05-30 15:05:16 -0700

    circless

--- main.py
+++ main.py
@@ -23,29 +23,33 @@ def paint(sourceImg, radii):
 
     blankCanvas = np.zeros((height,width,3), np.uint8)
     blankCanvas[:] = (0, 0, 0)
+    # color = int(blankCanvas[300, 300])
+    # print(color)
     cv2.namedWindow("blankCanvas");
     cv2.moveWindow("blankCanvas", 40, 30)  # Move it to (40,30)
 
-    cv2.circle(blankCanvas,(100, 300), 100, (120, 100, 50), -1)
-    # for i in range(width):  # for every col:
-    #     for j in range(height):  # For every row
-    #         blankCanvas[i, j] = (100, 100, 100)  # set the colour accordingly
+
+    for i in range(0, height, 20):  # for every col:
+        for j in range(0, width, 20):  # For every row
+             #blankCanvas[j, i] = (255, 255, 255)  # set the colour accordingly
+             cv2.circle(sourceImg, (j, i), 5, (i, j, 1), -1)
 
 
     cv2.imshow("blankCanvas",blankCanvas)
     cv2.waitKey(0)
     cv2.destroyAllWindows()
 
+    i = 0
 
-    # while i < len(radii):
-    #     print(radii[i])
-    #     blurImg = cv2.blur(sourceImg, (radii[i], radii[i]))
-    #     cv2.namedWindow("blurred image");
-    #     cv2.moveWindow("blurred image", 40, 30)  # Move it to (40,30)
-    #     cv2.imshow('blurred image', blurImg)
-    #     cv2.waitKey(0)
-    #     cv2.destroyAllWindows()
-    #     i += 1
+    while i < len(radii):
+        print(radii[i])
+        blurImg = cv2.blur(sourceImg, (radii[i], radii[i]))
+        cv2.namedWindow("blurred image");
+        cv2.moveWindow("blurred image", 40, 30)  # Move it to (40,30)
+        cv2.imshow('blurred image', blurImg)
+        cv2.waitKey(0)
+        cv2.destroyAllWindows()
+        i += 1
 
 
 
